c1d33eee2046a0bbdaeb7e03d8a563c71ec0db41
clementmariebrisson <clement.marie-brisson2.auditeur@lecnam.net>
2023-03-08 13:36:47 +0100

    Discrimination Imange | reshape modifications

--- Reseaux de neurones/main_discrimination_image.py
+++ Reseaux de neurones/main_discrimination_image.py
@@ -16,19 +16,19 @@ from sklearn.model_selection import train_test_split
 
 def import_measurements(filename):
     # Read the JCD measurements from the excel file
-    jcd_df = pd.read_excel(filename, sheet_name='WangSignaturesJCD')
+    jcd_df = pd.read_excel(filename, sheet_name='WangSignaturesJCD', header=None)
 
     # Read the PHOG measurements from the excel file
-    phog_df = pd.read_excel(filename, sheet_name='WangSignaturesPHOG')
+    phog_df = pd.read_excel(filename, sheet_name='WangSignaturesPHOG', header=None)
 
     # Read the CEDD measurements from the excel file
-    cedd_df = pd.read_excel(filename, sheet_name='WangSignaturesCEDD')
+    cedd_df = pd.read_excel(filename, sheet_name='WangSignaturesCEDD', header=None)
 
     # Read the FCTH measurements from the excel file
-    fcth_df = pd.read_excel(filename, sheet_name='WangSignaturesFCTH')
+    fcth_df = pd.read_excel(filename, sheet_name='WangSignaturesFCTH', header=None)
 
     # Read the Fuzzy Color Histogram measurements from the excel file
-    fuzzy_df = pd.read_excel(filename, sheet_name='WangSignaturesFuzzyColorHistogr')
+    fuzzy_df = pd.read_excel(filename, sheet_name='WangSignaturesFuzzyColorHistogr', header=None)
 
     # Concatenate the dataframes into a single dataframe
     data_df = pd.concat([jcd_df, phog_df, cedd_df, fcth_df, fuzzy_df], axis=1)
@@ -57,14 +57,17 @@ def import_measurements(filename):
         else:
             label.append('Plat')
         
-        # create label vector
-        label = np.zeros(999)
-        for i in range(10):
-            label[i*100:(i+1)*100] = i
+    # create label vector
+    label = np.zeros(1000)
+    for i in range(10):
+        label[i*100:(i+1)*100] = i
 
-        return jcd_df, phog_df, cedd_df, fcth_df, fuzzy_df, data_df, label
+    return jcd_df, phog_df, cedd_df, fcth_df, fuzzy_df, data_df, label
 
 def cnn_model_keras(data_df, label):
+    # Delete target column from dataframe
+    del data_df[0]
+
     # Convert the data dataframe and label vector to numpy arrays
     X = data_df.values
     y = np.array(label)
@@ -77,13 +80,29 @@ def cnn_model_keras(data_df, label):
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
     # Reshape the data to the format expected by the CNN
-    X_train = X_train.reshape((-1, 128, 128, 15))
-    X_val = X_val.reshape((-1, 128, 128, 15))
-    X_test = X_test.reshape((-1, 128, 128, 15))
+        # Decomposition of test_size in prime numbers
+            # test_size = 565 760
+            # 565 760 = 2^9 * 5 * 13 * 17
+            # 565 760 = 512 * 221 * 5
+    X_train = X_train.reshape((-1, 512, 221, 5))
+        # Decomposition of test_size in prime numbers
+            # X_val = 141 440
+            # 141 440 = 2^7 * 5 * 13 * 17
+            # 141 440 = 128 * 221 * 5
+    X_val = X_val.reshape((-1, 128, 221, 5))
+        # Decomposition of X_test in prime numbers
+            # X_val = 176 800
+            # 176 800 = 2^5 * 5^2 * 13 * 17
+            # 176 800 = 40 * 127 * 35
+    X_test = X_test.reshape((-1, 160, 221, 5))
+
+    X_train = np.asarray(X_train).astype('float32')
+    y_train = np.asarray(y_train).astype('float32')
 
     # Build the CNN model
+    print("\t○\tBuild the model")
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 15)))
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(512, 221, 5)))
     model.add(MaxPooling2D((2, 2)))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2, 2)))
@@ -94,16 +113,15 @@ def cnn_model_keras(data_df, label):
     model.add(Dense(10, activation='softmax'))
 
     # Compile the model
-    model.compile(optimizer=Adam(lr=0.001),
-                loss='categorical_crossentropy',
-                metrics=['accuracy'])
+    model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+    print("\t○\tModel requirements")
+    [print(i.shape, i.dtype) for i in model.inputs]
+    [print(o.shape, o.dtype) for o in model.outputs]
+    [print(l.name, l.input_shape, l.dtype) for l in model.layers]
 
     # Train the model
-    history = model.fit(X_train, y_train,
-                        epochs=50,
-                        batch_size=32,
-                        validation_data=(X_val, y_val),
-                        verbose=1)
+    history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_val, y_val), verbose=1)
 
     # Evaluate the model on the test set
     test_loss, test_acc = model.evaluate(X_test, y_test)
